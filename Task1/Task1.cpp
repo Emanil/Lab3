e41cc1354ad7979628f6a865be9cfaaaa7118a57
Emannil <emanil-9@student.ltu.se>
2019-12-01 18:54:42 +0100

    Started on 1 and 2. Added all other tasks too

--- Task1/Task1.cpp
+++ Task1/Task1.cpp
@@ -1,19 +1,19 @@
 #include <iostream>
 using namespace std;
 
-int len_of_string(char arr) {
-	for (int i = 0; i < arr; i++) {
-		if(i==arr-1)
-			return i;
+int len_of_string(char *arr) {
+	for (char i = 0; i < *arr-1; i++) {
+		cout << arr << endl;
+		return i;
 	}
 }
 
 int main() {
 	int len = 0;
-	char arr[] = "computer";
+	char arr[] = "four";
 	char* ptr = arr;
-	
-	len = len_of_string(*arr);
+
+	len = len_of_string(ptr);
 
 	cout << "The lenght is: " << len << endl;
 }
---
+++ Task2/Task2.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+int average_salary() {
+
+}
+
+int main() {
+
+}
